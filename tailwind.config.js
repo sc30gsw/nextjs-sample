a1bebcc0146aff784383ad34e6430b96e11900fe
sc30gsw <haidoushi875@gmail.com>
2023-07-28 06:53:26 +0900

    jitのエラー解消

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
