b62c4b4381ddfe5f44efbf6c6e8067621a3b15ff
sc30gsw <haidoushi875@gmail.com>
2023-07-28 06:53:20 +0900

    処理の書き換え

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head title="Next Sample" />
+      <Head title="Next Sample">
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Noto+Sans:wght@200&family=Poppins:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,14 +8,13 @@ import { useEffect } from 'react'
 
 import type { Content } from '@/types/content'
 import type { Post } from '@/types/post'
-import { notojp } from '@/utils/font'
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 })
 
 type StaticProps = {
-  post: Post | null
+  posts: Post[]
 }
 
 export const getStaticProps: GetStaticProps<StaticProps> = async () => {
@@ -42,182 +41,178 @@ export const getStaticProps: GetStaticProps<StaticProps> = async () => {
     ],
   })
 
-  const page = database.results[1]
+  const posts: Post[] = []
 
-  if (!page) {
-    return {
-      props: {
-        post: null,
-      },
-    }
-  }
+  const blockResponse = await Promise.all(
+    database.results.map((page) => {
+      return notion.blocks.children.list({
+        block_id: page.id,
+      })
+    }),
+  )
 
-  if (!('properties' in page)) {
-    return {
-      props: {
-        post: {
-          id: page.id,
-          title: null,
-          slug: null,
-          createdTs: null,
-          lastEditedTs: null,
-          contents: [],
-        },
-      },
-    }
-  }
-
-  let title: string | null = null
-  if (
-    page.properties['Name'].type === 'title' &&
-    Array.isArray(page.properties['Name'].title)
-  ) {
-    title = page.properties['Name'].title[0]?.plain_text ?? null
-  }
-
-  let slug: string | null = null
-  if (
-    page.properties['Slug'].type === 'rich_text' &&
-    Array.isArray(page.properties['Slug'].rich_text)
-  ) {
-    console.log(page.properties['Slug'].rich_text[0]?.plain_text)
-    slug = page.properties['Slug'].rich_text[0]?.plain_text ?? null
-  }
-
-  const blocks = await notion.blocks.children.list({
-    block_id: page.id,
-  })
-
-  const contents: Content[] = []
-  blocks.results.forEach((block) => {
-    if (!('type' in block)) {
+  database.results.forEach((page, index) => {
+    if (!('properties' in page)) {
+      posts.push({
+        id: page.id,
+        title: null,
+        slug: null,
+        createdTs: null,
+        lastEditedTs: null,
+        contents: [],
+      })
       return
     }
 
-    switch (block.type) {
-      case 'paragraph':
-        contents.push({
-          type: 'paragraph',
-          text: block.paragraph.rich_text[0]?.plain_text ?? null,
-        })
-        break
-
-      case 'heading_2':
-        contents.push({
-          type: 'heading_2',
-          text: block.heading_2.rich_text[0]?.plain_text ?? null,
-        })
-
-        break
-
-      case 'heading_3':
-        contents.push({
-          type: 'heading_3',
-          text: block.heading_3.rich_text[0]?.plain_text ?? null,
-        })
-
-        break
-
-      case 'quote':
-        contents.push({
-          type: 'quote',
-          text: block.quote.rich_text[0]?.plain_text ?? null,
-        })
-        break
-
-      case 'code':
-        contents.push({
-          type: 'code',
-          text: block.code.rich_text[0]?.plain_text ?? null,
-          language: block.code.language,
-        })
-        break
+    let title: string | null = null
+    if (
+      page.properties['Name'].type === 'title' &&
+      Array.isArray(page.properties['Name'].title)
+    ) {
+      title = page.properties['Name'].title[0]?.plain_text ?? null
     }
+
+    let slug: string | null = null
+    if (
+      page.properties['Slug'].type === 'rich_text' &&
+      Array.isArray(page.properties['Slug'].rich_text)
+    ) {
+      slug = page.properties['Slug'].rich_text[0]?.plain_text ?? null
+    }
+
+    const blocks = blockResponse[index]
+    const contents: Content[] = []
+    blocks.results.forEach((block) => {
+      if (!('type' in block)) {
+        return
+      }
+
+      switch (block.type) {
+        case 'paragraph':
+          contents.push({
+            type: 'paragraph',
+            text: block.paragraph.rich_text[0]?.plain_text ?? null,
+          })
+          break
+
+        case 'heading_2':
+          contents.push({
+            type: 'heading_2',
+            text: block.heading_2.rich_text[0]?.plain_text ?? null,
+          })
+
+          break
+
+        case 'heading_3':
+          contents.push({
+            type: 'heading_3',
+            text: block.heading_3.rich_text[0]?.plain_text ?? null,
+          })
+
+          break
+
+        case 'quote':
+          contents.push({
+            type: 'quote',
+            text: block.quote.rich_text[0]?.plain_text ?? null,
+          })
+          break
+
+        case 'code':
+          contents.push({
+            type: 'code',
+            text: block.code.rich_text[0]?.plain_text ?? null,
+            language: block.code.language,
+          })
+          break
+      }
+    })
+    posts.push({
+      id: page.id,
+      title,
+      slug,
+      createdTs: (page as any).created_time,
+      lastEditedTs: (page as any).last_edited_time,
+      contents,
+    })
   })
-
-  const post: Post = {
-    id: page.id,
-    title,
-    slug,
-    createdTs: (page as any).created_time,
-    lastEditedTs: (page as any).last_edited_time,
-    contents,
-  }
-
-  console.dir(post, { depth: null })
-
-  return { props: { post } }
+  return { props: { posts } }
 }
 
-const Home: NextPage<StaticProps> = ({ post }) => {
+const Home: NextPage<StaticProps> = ({ posts }) => {
   useEffect(() => {
     prism.highlightAll()
   }, [])
 
-  if (!post) return
+  if (!posts) return
 
   return (
-    <div className={`m-auto min-h-[100vh] max-w-[800px] ${notojp.className}`}>
-      <div className="mb-4 p-2">
-        <h1 className="my-4 text-2xl font-bold">{post.title}</h1>
-        <div className="mb-2 flex justify-end">
-          <div>
-            <div className="mb-1 text-sm">
-              作成日時: {dayjs(post.createdTs).format('YYYY-MM-DD HH:mm;ss')}
-            </div>
-            <div className="mb-1 text-sm">
-              更新日時: {dayjs(post.lastEditedTs).format('YYYY-MM-DD HH:mm;ss')}
+    <div className="m-auto min-h-[100vh] max-w-[800px]">
+      {posts.map((post) => (
+        <div className="mb-4 p-2" key={post.id}>
+          <h1 className="my-4 text-2xl font-bold">{post.title}</h1>
+          <div className="mb-2 flex justify-end">
+            <div>
+              <div className="mb-1 text-sm">
+                作成日時: {dayjs(post.createdTs).format('YYYY-MM-DD HH:mm;ss')}
+              </div>
+              <div className="mb-1 text-sm">
+                更新日時:{' '}
+                {dayjs(post.lastEditedTs).format('YYYY-MM-DD HH:mm;ss')}
+              </div>
             </div>
           </div>
+          <div>
+            {post.contents.map((content, index) => {
+              const key = `${post.id}_${index}`
+              switch (content.type) {
+                case 'heading_2':
+                  return (
+                    <h2 key={key} className="mx-auto my-2 text-xl font-medium">
+                      {content.text}
+                    </h2>
+                  )
+
+                case 'heading_3':
+                  return (
+                    <p key={key} className="mx-auto my-2 text-lg font-medium">
+                      {content.text}
+                    </p>
+                  )
+
+                case 'paragraph':
+                  return (
+                    <p key={key} className="leading-6">
+                      {content.text}
+                    </p>
+                  )
+
+                case 'code':
+                  return (
+                    <pre
+                      key={key}
+                      // prismのシンタックスハイライトのクラス(lang-言語名)
+                      // eslint-disable-next-line tailwindcss/no-custom-classname
+                      className={`lang-${content.language} mx-auto my-4 leading-6`}
+                    >
+                      <code>{content.text}</code>
+                    </pre>
+                  )
+
+                case 'quote':
+                  return (
+                    <blockquote
+                      key={key}
+                      className="m-0 border-l-2 border-custom-gray px-4 py-0 italic leading-6 text-custom-gray"
+                    >
+                      {content.text}
+                    </blockquote>
+                  )
+              }
+            })}
+          </div>
         </div>
-        <div>
-          {post.contents.map((content, index) => {
-            const key = `${post.id}_${index}`
-            switch (content.type) {
-              case 'heading_2':
-                return (
-                  <h2 key={key} className="mx-auto my-2 text-xl font-medium">
-                    {content.text}
-                  </h2>
-                )
-
-              case 'heading_3':
-                return (
-                  <p key={key} className="mx-auto my-2 text-lg font-medium">
-                    {content.text}
-                  </p>
-                )
-
-              case 'paragraph':
-                return (
-                  <p key={key} className="leading-6">
-                    {content.text}
-                  </p>
-                )
-
-              case 'code':
-                return (
-                  <pre
-                    // prismのシンタックスハイライトのクラス(lang-言語名)
-                    className={`lang-${content.language} mx-auto my-4 leading-6`}
-                  >
-                    <code>{content.text}</code>
-                  </pre>
-                )
-
-              case 'quote':
-                return (
-                  <blockquote
-                    key={key}
-                    className="m-0 border-l-2 border-custom-gray px-4 py-0 italic leading-6 text-custom-gray"
-                  >
-                    {content.text}
-                  </blockquote>
-                )
-            }
-          })}
-        </div>
-      </div>
+      ))}
     </div>
   )
 }
