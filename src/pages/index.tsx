0c97f14aaa72cc60546e94acdaa8b6bed1a6ed4c
sc30gsw <haidoushi875@gmail.com>
2023-07-28 02:26:51 +0900

    データ取得

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,33 @@ export const getStaticProps: GetStaticProps<{}> = async () => {
   const database = await notion.databases.query({
     // DB接続
     database_id: process.env.NOTION_DATABASE_ID as string,
+    filter: {
+      and: [
+        {
+          // Publishedがtrueのフィールドのみ抽出
+          property: 'Published',
+          checkbox: {
+            equals: true,
+          },
+        },
+      ],
+    },
+    sorts: [
+      {
+        // ページの作成日時の降順
+        timestamp: 'created_time',
+        direction: 'descending',
+      },
+    ],
   })
 
   console.dir(database, { depth: null })
+
+  const blocks = await notion.blocks.children.list({
+    block_id: database.results[1]?.id,
+  })
+
+  console.dir(blocks, { depth: null })
 
   return { props: {} }
 }
