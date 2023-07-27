99354a016c534bc571d17a8777a4bde5ebfa26f1
sc30gsw <haidoushi875@gmail.com>
2023-07-28 04:39:01 +0900

    ページの作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
+import 'prismjs/themes/prism-tomorrow.css'
+
 import { Client } from '@notionhq/client'
+import dayjs from 'dayjs'
 import type { GetStaticProps, NextPage } from 'next'
+import prism from 'prismjs'
+import { useEffect } from 'react'
 
 import type { Content } from '@/types/content'
 import type { Post } from '@/types/post'
@@ -145,11 +150,73 @@ export const getStaticProps: GetStaticProps<StaticProps> = async () => {
 }
 
 const Home: NextPage<StaticProps> = ({ post }) => {
+  useEffect(() => {
+    prism.highlightAll()
+  }, [])
+
   if (!post) return
+
   return (
     <div className={`m-auto min-h-[100vh] max-w-[800px] ${notojp.className}`}>
       <div className="mb-4 p-2">
         <h1 className="my-4 text-2xl font-bold">{post.title}</h1>
+        <div className="mb-2 flex justify-end">
+          <div>
+            <div className="mb-1 text-sm">
+              作成日時: {dayjs(post.createdTs).format('YYYY-MM-DD HH:mm;ss')}
+            </div>
+            <div className="mb-1 text-sm">
+              更新日時: {dayjs(post.lastEditedTs).format('YYYY-MM-DD HH:mm;ss')}
+            </div>
+          </div>
+        </div>
+        <div>
+          {post.contents.map((content, index) => {
+            const key = `${post.id}_${index}`
+            switch (content.type) {
+              case 'heading_2':
+                return (
+                  <h2 key={key} className="mx-auto my-2 text-xl font-medium">
+                    {content.text}
+                  </h2>
+                )
+
+              case 'heading_3':
+                return (
+                  <p key={key} className="mx-auto my-2 text-lg font-medium">
+                    {content.text}
+                  </p>
+                )
+
+              case 'paragraph':
+                return (
+                  <p key={key} className="leading-6">
+                    {content.text}
+                  </p>
+                )
+
+              case 'code':
+                return (
+                  <pre
+                    // prismのシンタックスハイライトのクラス(lang-言語名)
+                    className={`lang-${content.language} mx-auto my-4 leading-6`}
+                  >
+                    <code>{content.text}</code>
+                  </pre>
+                )
+
+              case 'quote':
+                return (
+                  <blockquote
+                    key={key}
+                    className="m-0 border-l-2 border-custom-gray px-4 py-0 italic leading-6 text-custom-gray"
+                  >
+                    {content.text}
+                  </blockquote>
+                )
+            }
+          })}
+        </div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,23 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  options: {
+    safelist: {
+      standard: [/^language-/, /^prism-/, /^pre/, /^code/],
+    },
+  },
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'custom-gray': '#d1d5db',
+      },
+      borderColor: {
+        'custom-gray': '#d1d5db',
       },
     },
   },
